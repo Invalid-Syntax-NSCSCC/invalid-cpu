6c5bfb6567512dd84cc6c222c1f64870f4d518c4
Takehana <cowpowermax@pm.me>
2023-03-07 19:11:47 +0800

    fix: make script more robust

--- scripts/modify_verilog.sh
+++ scripts/modify_verilog.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-sed -E 's/CpuTop/core_top/ ;
-        s/input[[:space:]]+clock/input aclk/ ;
-        s/input[[:space:]]+reset/input aresetn/ ;
-        s/\x29;/&\nwire clock;\nassign clock = aclk;\nreg reset;\nalways @(posedge aclk) reset <= ~aresetn;\n/' $1/CpuTop.v > $1/CpuTop_edited.v
+perl -0777 -pe 's/CpuTop/core_top/ ;
+                s/input[[:space:]]+clock/input aclk/ ;
+                s/input[[:space:]]+reset/input aresetn/ ;
+                s/\)\;\n/\)\;\nwire clock;\nassign clock = aclk;\nreg reset;\nalways @(posedge aclk) reset <= ~aresetn;\n/' $1/CpuTop.v > $1/CpuTop_edited.v
