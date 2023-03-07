59318e2c23ab56ca8a89a3b8988c94af41e2b420
Takehana <cowpowermax@pm.me>
2023-03-07 21:07:03 +0800

    fix: env on macOS

--- scripts/modify_verilog.sh
+++ scripts/modify_verilog.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-perl -0777 -pe 's/CpuTop/core_top/ ;
+env LC_ALL=C LANG=C perl -0777 -pe 's/CpuTop/core_top/ ;
                 s/input[[:space:]]+clock/input aclk/ ;
                 s/input[[:space:]]+reset/input aresetn/ ;
                 s/\)\;\n/\)\;\nwire clock;\nassign clock = aclk;\nreg reset;\nalways @(posedge aclk) reset <= ~aresetn;\n/' $1/CpuTop.v > $1/CpuTop_edited.v
