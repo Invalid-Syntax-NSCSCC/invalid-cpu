4f73ee6b97034aa721606b45cd1257feb0204b52
invalid2 <noreply@>
2023-03-15 01:57:43 +0800

    fix: restore ports changes in CpuTop

--- src/src/CpuTop.scala
+++ src/src/CpuTop.scala
@@ -1,9 +1,7 @@
-import axi.AxiMaster
 import chisel3._
 import chisel3.experimental.FlatIO
 import chisel3.util._
 import common.RegFile
-import common.bundles.AxiMasterPort
 import frontend.InstQueue
 import pipeline.dispatch.{IssueStage, RegReadStage, Scoreboard}
 import pipeline.execution.ExeStage
@@ -13,7 +11,53 @@ class CpuTop extends Module {
   val io = FlatIO(new Bundle {
     val intrpt = Input(UInt(8.W))
 
-    val axiMaster = new AxiMasterPort
+    // AXI interface is as follow
+
+    // Read request
+    val arid    = Output(UInt(4.W))
+    val araddr  = Output(UInt(32.W))
+    val arlen   = Output(UInt(8.W))
+    val arsize  = Output(UInt(3.W))
+    val arburst = Output(UInt(2.W))
+    val arlock  = Output(UInt(2.W))
+    val arcache = Output(UInt(4.W))
+    val arprot  = Output(UInt(3.W))
+    val arvalid = Output(Bool())
+    val arready = Input(Bool())
+
+    // Read back
+    val rid    = Input(UInt(4.W))
+    val rdata  = Input(UInt(32.W))
+    val rresp  = Input(UInt(2.W))
+    val rlast  = Input(Bool())
+    val rvalid = Input(Bool())
+    val rready = Output(Bool())
+
+    // Write request
+    val awid    = Output(UInt(4.W))
+    val awaddr  = Output(UInt(32.W))
+    val awlen   = Output(UInt(8.W))
+    val awsize  = Output(UInt(3.W))
+    val awburst = Output(UInt(2.W))
+    val awlock  = Output(UInt(2.W))
+    val awcache = Output(UInt(4.W))
+    val awprot  = Output(UInt(3.W))
+    val awvalid = Output(Bool())
+    val awready = Input(Bool())
+
+    // Write data
+    val wid    = Output(UInt(4.W))
+    val wdata  = Output(UInt(32.W))
+    val wstrb  = Output(UInt(4.W))
+    val wlast  = Output(Bool())
+    val wvalid = Output(Bool())
+    val wready = Input(Bool())
+
+    // Write back
+    val bid    = Input(UInt(4.W))
+    val bresp  = Input(UInt(2.W))
+    val bvalid = Input(Bool())
+    val bready = Output(Bool())
 
     val debug0_wb = new Bundle {
       val pc = Output(UInt(32.W))
@@ -30,7 +74,7 @@ class CpuTop extends Module {
 
   // TODO: Remove temporary test content
   val testReg = RegNext(true.B, false.B)
-  io.axiMaster.bready := testReg
+  io.bready := testReg
 
   val instQueue    = Module(new InstQueue)
   val issueStage   = Module(new IssueStage)
