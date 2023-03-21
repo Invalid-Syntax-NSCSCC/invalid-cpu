32c817f95a7c09ce0ebbcc13d77c18916e2f33f6
Takehana <cowpowermax@pm.me>
2023-03-21 16:04:09 +0800

    fix: refactor issue stage using state machine to make it work

--- src/src/pipeline/dispatch/IssueStage.scala
+++ src/src/pipeline/dispatch/IssueStage.scala
@@ -6,6 +6,7 @@ import pipeline.dispatch.bundles.{DecodeOutNdPort, DecodePort, InstInfoBundle, I
 import pipeline.dispatch.decode.{Decoder, Decoder_2R, Decoder_2RI12, Decoder_2RI16, Decoder_3R, Decoder_4R}
 import spec._
 import pipeline.ctrl.bundles.PipelineControlNDPort
+import spec.Param.{IssueStageState => State}
 
 class IssueStage(scoreChangeNum: Int = Param.scoreboardChangeNum) extends Module {
   val io = IO(new Bundle {
@@ -20,31 +21,70 @@ class IssueStage(scoreChangeNum: Int = Param.scoreboardChangeNum) extends Module
     val issuedInfoPort = Output(new IssuedInfoNdPort)
 
     // pipeline control signal
-    // `CtrlStage` -> `IssueStage`
+    // `Cu` -> `IssueStage`
     val pipelineControlPort = Input(new PipelineControlNDPort)
   })
-
-  // TODO: Refactor using state machine
 
   // Pass to the next stage in a sequential way
   val issuedInfoReg = RegInit(IssuedInfoNdPort.default)
   io.issuedInfoPort := issuedInfoReg
 
-  // Get next instruction if needed
-  val isNonBlocking   = WireDefault(true.B)
-  val isInstAvailable = WireDefault(io.fetchInstInfoPort.valid && isNonBlocking)
-  val instInfoReg     = RegEnable(io.fetchInstInfoPort.bits, InstInfoBundle.default, isInstAvailable)
-  io.fetchInstInfoPort.ready := isNonBlocking
+  // Start: state machine
+
+  /** State behaviors (mealy machine):
+    *   - Fallback: keep inst store reg, no issue, no fetch
+    *   - `nonBlocking`:
+    *     - If can fetch: store inst to reg
+    *       - If blocking: (nothing else)
+    *       - if still non-blocking: fetch, issue
+    *     - If cannot fetch: fetch
+    *   - `blocking`:
+    *     - If still blocking: (nothing else)
+    *     - If non-blocking: fetch, issue
+    *
+    * State behaviors (moore machine):
+    *   - `nonBlocking`: decode from port
+    *   - `blocking`: decode from inst store reg
+    *
+    * State transitions:
+    *   - `nonBlocking`: is blocking -> `blocking`, else `nonBlocking`
+    *   - `blocking`: is blocking -> `blocking`, else `nonBlocking`
+    */
+  val nextState = WireInit(State.nonBlocking)
+  val stateReg  = RegInit(State.nonBlocking)
+  stateReg := nextState
+
+  // State machine output (including fallback)
+  val instStoreReg = RegInit(InstInfoBundle.default)
+  instStoreReg := instStoreReg
+  val selectedInstInfo = WireDefault(InstInfoBundle.default)
+  val isIssue          = WireDefault(false.B)
+  val isFetch          = WireDefault(false.B)
+
+  // Connect state machine output to I/O
+  io.fetchInstInfoPort.ready := isFetch
+
+  // Implement output function (moore)
+  switch(stateReg) {
+    is(State.nonBlocking) {
+      selectedInstInfo := io.fetchInstInfoPort.bits
+    }
+    is(State.blocking) {
+      selectedInstInfo := instStoreReg
+    }
+  }
+
+  // Decode
 
   // Select a decoder
   val decoders = Seq(
     Module(new Decoder_2RI12),
     Module(new Decoder_2RI16),
     Module(new Decoder_2R),
-    Module(new Decoder_3R),
+//    Module(new Decoder_3R),
     Module(new Decoder_4R)
   )
-  decoders.foreach(_.io.inst := instInfoReg.inst)
+  decoders.foreach(_.io.instInfoPort := selectedInstInfo)
 
   val decoderWires = Wire(Vec(decoders.length, new DecodeOutNdPort))
   decoderWires.zip(decoders).foreach {
@@ -54,17 +94,55 @@ class IssueStage(scoreChangeNum: Int = Param.scoreboardChangeNum) extends Module
   val decoderIndex    = WireDefault(OHToUInt(Cat(decoderWires.map(_.isMatched).reverse)))
   val selectedDecoder = WireDefault(decoderWires(decoderIndex))
 
-  // Check scoreboard
-  val isScoreboardNonBlocking = WireDefault(selectedDecoder.info.gprReadPorts.map { port =>
-    !(port.en && io.regScores(port.addr))
-  }.reduce(_ && _))
-  isNonBlocking := isScoreboardNonBlocking && !io.pipelineControlPort.stall
-
+  // State machine input
+  /** Determine blocking:
+    *   - If stall signal: blocking
+    *   - If not stall:
+    *     - If inst valid:
+    *       - If no data hazard: non-blocking
+    *       - If data hazard: blocking
+    *     - If inst invalid:
+    *       - non-blocking
+    */
   val isInstValid = WireDefault(decoderWires.map(_.isMatched).reduce(_ || _))
-  val isNeedIssue = WireDefault(isInstAvailable && isInstValid) // TODO: Include in refactor
+  val isScoreboardBlocking = WireDefault(selectedDecoder.info.gprReadPorts.map { port =>
+    port.en && io.regScores(port.addr)
+  }.reduce(_ || _))
+  val isBlocking = WireDefault(
+    io.pipelineControlPort.stall || (
+      isInstValid && isScoreboardBlocking
+    )
+  )
+  val isLastCanFetchReg = RegNext(io.fetchInstInfoPort.valid, false.B)
+
+  // Implement output function (mealy)
+  switch(stateReg) {
+    is(State.nonBlocking) {
+      when(isLastCanFetchReg) {
+        instStoreReg := io.fetchInstInfoPort.bits
+        when(!isBlocking) {
+          isFetch := true.B
+          isIssue := true.B
+        }
+      }.otherwise {
+        isFetch := true.B
+      }
+    }
+    is(State.blocking) {
+      when(!isBlocking) {
+        isFetch := true.B
+        isIssue := true.B
+      }
+    }
+  }
+
+  // Next state function
+  nextState := Mux(isBlocking, State.blocking, State.nonBlocking)
+
+  // End: state machine
 
   // Fallback for no operation
-  issuedInfoReg.isValid              := isNeedIssue
+  issuedInfoReg.isValid              := isIssue
   issuedInfoReg.info                 := DontCare
   issuedInfoReg.info.gprWritePort.en := false.B
   io.occupyPorts.foreach { port =>
@@ -73,7 +151,7 @@ class IssueStage(scoreChangeNum: Int = Param.scoreboardChangeNum) extends Module
   }
 
   // Issue pre-execution instruction
-  when(isNeedIssue) {
+  when(isIssue) {
     // Indicate the occupation in scoreboard
     io.occupyPorts.zip(Seq(selectedDecoder.info.gprWritePort)).foreach {
       case (occupyPort, accessInfo) =>
