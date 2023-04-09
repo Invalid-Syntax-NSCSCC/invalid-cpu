214e49595b6c0ebb681474d93292f454766add32
Chrisqcwx <3075401625@qq.com>
2023-04-09 23:39:31 +0800

    refactor: remove some comment

--- src/test/src/DataForwardSpec.scala
+++ src/test/src/DataForwardSpec.scala
@@ -10,32 +10,32 @@ import scala.collection.immutable
 // import pipeline.dataforward.DataForwardStage
 import pipeline.dataforward.DataForwardStage
 
- object DataForwardSpec extends ChiselUtestTester {
-  val tests = Tests {
-    test("Test data forward module") {
-      testCircuit(new DataForwardStage(3), immutable.Seq(WriteVcdAnnotation)) { dataforward =>
-        def wp = dataforward.io.writePorts
-        def rp = dataforward.io.readPorts
-        rp(0).en.poke(true.B)
-        rp(1).en.poke(true.B)
-        rp(0).addr.poke(2)
-        rp(1).addr.poke(3)
+//  object DataForwardSpec extends ChiselUtestTester {
+//   val tests = Tests {
+//     test("Test data forward module") {
+//       testCircuit(new DataForwardStage(3), immutable.Seq(WriteVcdAnnotation)) { dataforward =>
+//         def wp = dataforward.io.writePorts
+//         def rp = dataforward.io.readPorts
+//         rp(0).en.poke(true.B)
+//         rp(1).en.poke(true.B)
+//         rp(0).addr.poke(2)
+//         rp(1).addr.poke(3)
 
-        wp(0).en .poke(true.B);wp(0).addr.poke(2); wp(0).data.poke(4)
-        wp(1).en .poke(true.B);wp(1).addr.poke(2); wp(1).data.poke(6)
-        wp(2).en .poke(true.B);wp(2).addr.poke(2); wp(2).data.poke(7)
+//         wp(0).en .poke(true.B);wp(0).addr.poke(2); wp(0).data.poke(4)
+//         wp(1).en .poke(true.B);wp(1).addr.poke(2); wp(1).data.poke(6)
+//         wp(2).en .poke(true.B);wp(2).addr.poke(2); wp(2).data.poke(7)
 
-        dataforward.clock.step(1)
-        wp(0).en .poke(true.B);wp(0).addr.poke(2); wp(0).data.poke(4)
-        wp(1).en .poke(true.B);wp(1).addr.poke(3); wp(1).data.poke(6)
-        wp(2).en .poke(false.B);wp(2).addr.poke(2); wp(2).data.poke(7)
-        dataforward.clock.step(1)
-        wp(0).en .poke(false.B);wp(0).addr.poke(2); wp(0).data.poke(4)
-        wp(1).en.poke( true.B);wp(1).addr.poke(3); wp(1).data.poke(6)
-        wp(2).en.poke( true.B);wp(2).addr.poke(2); wp(2).data.poke(7)
-        dataforward.clock.step(1)
+//         dataforward.clock.step(1)
+//         wp(0).en .poke(true.B);wp(0).addr.poke(2); wp(0).data.poke(4)
+//         wp(1).en .poke(true.B);wp(1).addr.poke(3); wp(1).data.poke(6)
+//         wp(2).en .poke(false.B);wp(2).addr.poke(2); wp(2).data.poke(7)
+//         dataforward.clock.step(1)
+//         wp(0).en .poke(false.B);wp(0).addr.poke(2); wp(0).data.poke(4)
+//         wp(1).en.poke( true.B);wp(1).addr.poke(3); wp(1).data.poke(6)
+//         wp(2).en.poke( true.B);wp(2).addr.poke(2); wp(2).data.poke(7)
+//         dataforward.clock.step(1)
 
-      }
-    }
-  }
- }
+//       }
+//     }
+//   }
+//  }
