70a39188342b19d99918a7c5d8a20efe73b44465
Chrisqcwx <3075401625@qq.com>
2023-04-21 15:36:02 +0800

    refactor: tidy up the test files' path

--- src/test/src/BiIssueSpecs/MinFinderSpec.scala
+++ src/test/src/BiIssueSpecs/MinFinderSpec.scala
@@ -16,21 +16,21 @@ object MinFinderSpec extends ChiselUtestTester {
   val tests = Tests {
     test("Test MinFinder") {
       testCircuit(new MinFinder(num = 5, wordLength=4), Seq(WriteVcdAnnotation)) { 
-        minFinder =>
-        val numsSeq = Seq(
-            Seq(1.U,2.U, 4.U, 3.U,5.U),
-            Seq(7.U,3.U, 4.U, 3.U,5.U),
-            Seq(8.U,2.U, 4.U, 3.U,5.U)
-        )
-        val masks = (Seq.fill(5)(true.B))
+        minFinder => if (true) {
+          val numsSeq = Seq(
+              Seq(1.U,2.U, 4.U, 3.U,5.U),
+              Seq(7.U,3.U, 4.U, 3.U,5.U),
+              Seq(8.U,2.U, 4.U, 3.U,5.U)
+          )
+          val masks = (Seq.fill(5)(true.B))
 
-        minFinder.io.masks.zip(masks).foreach{case (dst, src) => dst.poke(src)}
-        numsSeq.foreach{nums => 
-            minFinder.io.values.zip(nums).foreach{case (dst, src) => dst poke src}
-            println(minFinder.io.index.peek())
-            minFinder.clock.step(1)
+          minFinder.io.masks.zip(masks).foreach{case (dst, src) => dst.poke(src)}
+          numsSeq.foreach{nums => 
+              minFinder.io.values.zip(nums).foreach{case (dst, src) => dst poke src}
+              println(minFinder.io.index.peek())
+              minFinder.clock.step(1)
+          }
         }
-          
       }
     }
   }
