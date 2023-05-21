476b6a239fdf2b27ecc326946d8a93302668a8d5
Chrisqcwx <3075401625@qq.com>
2023-05-21 21:14:27 +0800

    refactor: refactor Cu

--- src/src/control/Cu.scala
+++ src/src/control/Cu.scala
@@ -66,23 +66,27 @@ class Cu(
 
   io.pipelineControlPorts.foreach(_ := PipelineControlNDPort.default)
   // `ExeStage` --stall--> `IssueStage`, `RegReadStage`, `ExeStage` (STALL ITSELF)
-  when(io.exeStallRequest) {
-    Seq(PipelineStageIndex.issueStage, PipelineStageIndex.regReadStage, PipelineStageIndex.exeStage)
-      .map(io.pipelineControlPorts(_))
-      .foreach(_.stall := true.B)
-  }
+  io.pipelineControlPorts(PipelineStageIndex.instQueue).stall    := io.memStallRequest && io.exeStallRequest
+  io.pipelineControlPorts(PipelineStageIndex.regReadStage).stall := io.memStallRequest && io.exeStallRequest
+  io.pipelineControlPorts(PipelineStageIndex.exeStage).stall     := io.memStallRequest && io.exeStallRequest
+  io.pipelineControlPorts(PipelineStageIndex.memStage).stall     := io.memStallRequest
+  // when(io.exeStallRequest) {
+  //   Seq(PipelineStageIndex.issueStage, PipelineStageIndex.regReadStage, PipelineStageIndex.exeStage)
+  //     .map(io.pipelineControlPorts(_))
+  //     .foreach(_.stall := true.B)
+  // }
 
-  // `AddrTransStage` --stall--> `IssueStage`, `RegReadStage`, `ExeStage`, `AddrTransStage`  (STALL ITSELF)
-  when(io.memStallRequest) {
-    Seq(
-      PipelineStageIndex.issueStage,
-      PipelineStageIndex.regReadStage,
-      PipelineStageIndex.exeStage,
-      PipelineStageIndex.memStage
-    )
-      .map(io.pipelineControlPorts(_))
-      .foreach(_.stall := true.B)
-  }
+  // // `AddrTransStage` --stall--> `IssueStage`, `RegReadStage`, `ExeStage`, `AddrTransStage`  (STALL ITSELF)
+  // when(io.memStallRequest) {
+  //   Seq(
+  //     PipelineStageIndex.issueStage,
+  //     PipelineStageIndex.regReadStage,
+  //     PipelineStageIndex.exeStage,
+  //     PipelineStageIndex.memStage
+  //   )
+  //     .map(io.pipelineControlPorts(_))
+  //     .foreach(_.stall := true.B)
+  // }
 
   /** clear
     *
