ad61d8b693862312e126313758554f6f2e9778b8
rewired <39949564+rewired-gh@users.noreply.github.com>
2023-05-22 05:08:15 -0400

    feat: make 'rebasing' safer (#61)

--- scripts/refresh_branch.sh
+++ scripts/refresh_branch.sh
@@ -2,6 +2,21 @@
 
 set -e
 
+echo
+while true; do
+  echo "Are you wish to refresh branch \`$1'? This operation will DISCARD all your unmerged work on branch \`$1' and CANNOT be reverted!"
+    read -r yn
+    case $yn in
+        [Yy]* )
+          break
+          ;;
+        * )
+          echo "Operation aborted."
+          exit
+          ;;
+    esac
+done
+
 git switch main
 git pull
 git branch -D "$1"
