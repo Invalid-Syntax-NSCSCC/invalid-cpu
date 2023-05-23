8c9f0889a987b721e98fa1f2f191ad2f694d8d79
Chrisqcwx <3075401625@qq.com>
2023-05-23 16:15:23 +0800

    refactor: csr

--- src/src/control/Csr.scala
+++ src/src/control/Csr.scala
@@ -26,35 +26,6 @@ class Csr(
     // `Csr` <-> `IssueStage` / `RegReadStage` ???
     val readPorts = Vec(Param.csrRegsReadNum, new CsrReadPort)
 
-    val difftest = if (isDiffTest) Some(Output(new Bundle {
-      val crmd      = UInt(32.W)
-      val prmd      = UInt(32.W)
-      val ectl      = UInt(32.W)
-      val estat     = new EstatBundle
-      val era       = UInt(32.W)
-      val badv      = UInt(32.W)
-      val eentry    = UInt(32.W)
-      val tlbidx    = UInt(32.W)
-      val tlbehi    = UInt(32.W)
-      val tlbelo0   = UInt(32.W)
-      val tlbelo1   = UInt(32.W)
-      val asid      = UInt(32.W)
-      val save0     = UInt(32.W)
-      val save1     = UInt(32.W)
-      val save2     = UInt(32.W)
-      val save3     = UInt(32.W)
-      val tid       = UInt(32.W)
-      val tcfg      = UInt(32.W)
-      val tval      = UInt(32.W)
-      val ticlr     = UInt(32.W)
-      val llbctl    = UInt(32.W)
-      val tlbrentry = UInt(32.W)
-      val dmw0      = UInt(32.W)
-      val dmw1      = UInt(32.W)
-      val pgdl      = UInt(32.W)
-      val pgdh      = UInt(32.W)
-    }))
-    else None
   })
 
   // Util: view UInt as Bundle
@@ -84,11 +55,6 @@ class Csr(
       zeroWord
     )
   }
-
-  // 输出
-  io.csrValues.era       := csrRegs(spec.Csr.Index.era)
-  io.csrValues.eentry    := csrRegs(spec.Csr.Index.eentry)
-  io.csrValues.tlbrentry := csrRegs(spec.Csr.Index.tlbrentry)
 
   // 软件写csrRegs
   // 保留域断断续续的样子真是可爱捏
@@ -353,36 +319,35 @@ class Csr(
     } // 为0时停止计数
   }
 
-  // Difftest
-  io.difftest match {
-    case Some(dt) =>
-      dt.crmd := crmd.out.asUInt
-      dt.prmd := prmd.out.asUInt
-      // TODO: `ectl` is not implemented
-      dt.ectl      := DontCare
-      dt.estat     := estat.out
-      dt.era       := era.out.asUInt
-      dt.badv      := badv.out.asUInt
-      dt.eentry    := eentry.out.asUInt
-      dt.tlbidx    := tlbidx.out.asUInt
-      dt.tlbehi    := tlbehi.out.asUInt
-      dt.tlbelo0   := tlbelo0.out.asUInt
-      dt.tlbelo1   := tlbelo1.out.asUInt
-      dt.asid      := asid.out.asUInt
-      dt.save0     := saves(0).out.asUInt
-      dt.save1     := saves(1).out.asUInt
-      dt.save2     := saves(2).out.asUInt
-      dt.save3     := saves(3).out.asUInt
-      dt.tid       := tid.out.asUInt
-      dt.tcfg      := tcfg.out.asUInt
-      dt.tval      := tval.out.asUInt
-      dt.ticlr     := ticlr.out.asUInt
-      dt.llbctl    := llbctl.out.asUInt
-      dt.tlbrentry := tlbrentry.out.asUInt
-      dt.dmw0      := dmw0.out.asUInt
-      dt.dmw1      := dmw1.out.asUInt
-      dt.pgdl      := pgdl.out.asUInt
-      dt.pgdh      := pgdh.out.asUInt
-    case _ =>
-  }
+  // output
+  io.csrValues.crmd      := csrRegs(spec.Csr.Index.crmd).asTypeOf(io.csrValues.crmd)
+  io.csrValues.prmd      := csrRegs(spec.Csr.Index.prmd).asTypeOf(io.csrValues.prmd)
+  io.csrValues.euen      := csrRegs(spec.Csr.Index.euen).asTypeOf(io.csrValues.euen)
+  io.csrValues.ecfg      := csrRegs(spec.Csr.Index.ecfg).asTypeOf(io.csrValues.ecfg)
+  io.csrValues.estat     := csrRegs(spec.Csr.Index.estat).asTypeOf(io.csrValues.estat)
+  io.csrValues.era       := csrRegs(spec.Csr.Index.era).asTypeOf(io.csrValues.era)
+  io.csrValues.badv      := csrRegs(spec.Csr.Index.badv).asTypeOf(io.csrValues.badv)
+  io.csrValues.eentry    := csrRegs(spec.Csr.Index.eentry).asTypeOf(io.csrValues.eentry)
+  io.csrValues.tlbidx    := csrRegs(spec.Csr.Index.tlbidx).asTypeOf(io.csrValues.tlbidx)
+  io.csrValues.tlbehi    := csrRegs(spec.Csr.Index.tlbehi).asTypeOf(io.csrValues.tlbehi)
+  io.csrValues.tlbelo0   := csrRegs(spec.Csr.Index.tlbelo0).asTypeOf(io.csrValues.tlbelo0)
+  io.csrValues.tlbelo1   := csrRegs(spec.Csr.Index.tlbelo1).asTypeOf(io.csrValues.tlbelo1)
+  io.csrValues.asid      := csrRegs(spec.Csr.Index.asid).asTypeOf(io.csrValues.asid)
+  io.csrValues.pgdl      := csrRegs(spec.Csr.Index.pgdl).asTypeOf(io.csrValues.pgdl)
+  io.csrValues.pgdh      := csrRegs(spec.Csr.Index.pgdh).asTypeOf(io.csrValues.pgdh)
+  io.csrValues.pgd       := csrRegs(spec.Csr.Index.pgd).asTypeOf(io.csrValues.pgd)
+  io.csrValues.cpuid     := csrRegs(spec.Csr.Index.cpuid).asTypeOf(io.csrValues.cpuid)
+  io.csrValues.save0     := csrRegs(spec.Csr.Index.save0).asTypeOf(io.csrValues.save0)
+  io.csrValues.save1     := csrRegs(spec.Csr.Index.save1).asTypeOf(io.csrValues.save1)
+  io.csrValues.save2     := csrRegs(spec.Csr.Index.save2).asTypeOf(io.csrValues.save2)
+  io.csrValues.save3     := csrRegs(spec.Csr.Index.save3).asTypeOf(io.csrValues.save3)
+  io.csrValues.tid       := csrRegs(spec.Csr.Index.tid).asTypeOf(io.csrValues.tid)
+  io.csrValues.tcfg      := csrRegs(spec.Csr.Index.tcfg).asTypeOf(io.csrValues.tcfg)
+  io.csrValues.tval      := csrRegs(spec.Csr.Index.tval).asTypeOf(io.csrValues.tval)
+  io.csrValues.ticlr     := csrRegs(spec.Csr.Index.ticlr).asTypeOf(io.csrValues.ticlr)
+  io.csrValues.llbctl    := csrRegs(spec.Csr.Index.llbctl).asTypeOf(io.csrValues.llbctl)
+  io.csrValues.tlbrentry := csrRegs(spec.Csr.Index.tlbrentry).asTypeOf(io.csrValues.tlbrentry)
+  io.csrValues.dmw0      := csrRegs(spec.Csr.Index.dmw0).asTypeOf(io.csrValues.dmw0)
+  io.csrValues.dmw1      := csrRegs(spec.Csr.Index.dmw1).asTypeOf(io.csrValues.dmw1)
+
 }
