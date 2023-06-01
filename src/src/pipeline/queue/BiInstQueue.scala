7f164f64e036822fa6152701ce9f608b0cf32029
Takehana <cowpowermax@pm.me>
2023-06-01 11:34:55 +0800

    fix: CSR write implementation

--- src/src/pipeline/queue/BiInstQueue.scala
+++ src/src/pipeline/queue/BiInstQueue.scala
@@ -175,8 +175,7 @@ class BiInstQueue(
 
   io.dequeuePorts.lazyZip(selectedDecoders).lazyZip(decodeInstInfos).zipWithIndex.foreach {
     case ((dequeuePort, selectedDecoder, decodeInstInfo), index) =>
-      dequeuePort.bits.decode := selectedDecoder
-      // InstInfoNdPort.setDefault(dequeuePort.bits.instInfo)
+      dequeuePort.bits.decode        := selectedDecoder
       dequeuePort.bits.instInfo      := InstInfoNdPort.default
       dequeuePort.bits.instInfo.pc   := decodeInstInfo.pcAddr
       dequeuePort.bits.instInfo.inst := decodeInstInfo.inst
