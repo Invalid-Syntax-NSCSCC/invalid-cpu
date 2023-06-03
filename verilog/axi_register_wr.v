e034be62f997e423bad89a14ec0c9f46baa3fb08
Takehana <cowpowermax@pm.me>
2023-06-03 14:29:01 +0800

    fix: remove verilog clutters

--- verilog/axi_register_wr.v
+++ verilog/axi_register_wr.v
@@ -24,10 +24,6 @@ THE SOFTWARE.
 
 // Language: Verilog 2001
 
-`resetall
-`timescale 1ns / 1ps
-`default_nettype none
-
 /*
  * AXI4 register (write)
  */
@@ -687,5 +683,3 @@ end
 endgenerate
 
 endmodule
-
-`resetall
