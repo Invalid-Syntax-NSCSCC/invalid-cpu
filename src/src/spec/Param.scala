b5c73af1d00a6e229f57e2464808e77ee32e3a58
Chrisqcwx <3075401625@qq.com>
2023-06-23 15:34:14 +0800

    refactor: change some params

--- src/src/spec/Param.scala
+++ src/src/spec/Param.scala
@@ -20,7 +20,7 @@ object Param {
   val issueInstInfoMaxNum    = 2 // 发射数量
   val commitNum              = 2 // 单次提交数量
   val pipelineNum            = 3 // number of pipeline
-  val reservationStationDeep = 3 // 保留站深度
+  val reservationStationDeep = 4 // 保留站深度
   val csrRegsReadNum         = 1
   val csrRegsWriteNum        = 1
 
@@ -36,8 +36,8 @@ object Param {
     val exeOp  = log2Ceil(ExeInst.Op.count + 1).W
 
     object Rob {
-      val _length = 15
-      val id      = log2Ceil(_length + 1).W
+      val _length = 16
+      val id      = log2Ceil(_length).W
     }
 
     object Axi {
