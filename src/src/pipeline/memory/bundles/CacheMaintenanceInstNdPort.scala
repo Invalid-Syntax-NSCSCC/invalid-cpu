50529add2abc79b1086a5bdd29009c5084565851
Takehana <cowpowermax@pm.me>
2023-06-27 16:30:49 +0800

    feat: cache maintenance interface

--- src/src/pipeline/memory/bundles/CacheMaintenanceInstNdPort.scala
+++ src/src/pipeline/memory/bundles/CacheMaintenanceInstNdPort.scala
@@ -4,9 +4,9 @@ import chisel3._
 import chisel3.util._
 import spec._
 import pipeline.memory.enums.CacheMaintenanceTargetType
-import memory.bundles.CacheMaintenanceNdPort
+import memory.bundles.CacheMaintenanceControlNdPort
 
 class CacheMaintenanceInstNdPort extends Bundle {
-  val target = CacheMaintenanceTargetType()
-  val client = new CacheMaintenanceNdPort
+  val target  = CacheMaintenanceTargetType()
+  val control = new CacheMaintenanceControlNdPort
 }
