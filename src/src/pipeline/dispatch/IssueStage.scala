2e4dcf345d394b430e7a78e88f81e622cac3098c
Chrisqcwx <3075401625@qq.com>
2023-06-28 19:48:48 +0800

    feat: add ipe exception

--- src/src/pipeline/dispatch/IssueStage.scala
+++ src/src/pipeline/dispatch/IssueStage.scala
@@ -11,6 +11,7 @@ import pipeline.rob.bundles.{InstWbNdPort, RobReadRequestNdPort, RobReadResultNd
 import pipeline.rob.enums.RobDistributeSel
 import spec.Param.{csrIssuePipelineIndex, loadStoreIssuePipelineIndex}
 import spec._
+import control.enums.ExceptionPos
 
 // class FetchInstDecodeNdPort extends Bundle {
 //   val decode   = new DecodeOutNdPort
@@ -44,6 +45,8 @@ class IssueStagePeerPort(
 
   // `Cu` -> `IssueStage`
   val branchFlush = Input(Bool())
+
+  val plv = Input(UInt(2.W))
 }
 
 // dispatch & Reservation Stations
@@ -290,6 +293,26 @@ class IssueStage(
 
       out.bits.instInfo       := deqPort.bits.regReadPort.instInfo
       out.bits.instInfo.robId := deqPort.bits.robResult.robId
+
+      when(io.peer.get.plv =/= 0.U) {
+        when(
+          deqPort.bits.regReadPort.preExeInstInfo.isTlb ||
+            VecInit(
+              ExeInst.Op.csrrd,
+              ExeInst.Op.csrwr,
+              ExeInst.Op.csrxchg,
+              ExeInst.Op.ertn,
+              ExeInst.Op.idle,
+              ExeInst.Op.cacop
+            ).contains(deqPort.bits.regReadPort.preExeInstInfo.exeOp)
+        ) {
+          when(deqPort.bits.regReadPort.instInfo.exceptionPos =/= ExceptionPos.none) {
+            out.bits.instInfo.exceptionPos    := ExceptionPos.backend
+            out.bits.instInfo.exceptionRecord := Csr.ExceptionIndex.ipe
+          }
+        }
+      }
+
   }
 
   when(io.peer.get.branchFlush) {
