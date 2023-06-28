3b12693bb848fa2eb240f23598dc5f0f5793abe0
Chrisqcwx <92083012+Chrisqcwx@users.noreply.github.com>
2023-06-28 13:16:16 +0800

    refactor with parallel commit & no depend on difftest info (#178)
    * refactor csr / branchSuccess / idle / store
    
    * refactor exception forbidParallelCommit
    
    * refactor: remove some comment
    
    * fix: default of InstInfoNdPort when no difftest

--- src/src/pipeline/commit/bundles/InstInfoNdPort.scala
+++ src/src/pipeline/commit/bundles/InstInfoNdPort.scala
@@ -18,12 +18,11 @@ class InstInfoNdPort extends Bundle {
   val vaddr           = UInt(Width.Mem.addr)
   val needCsr         = Bool()
   val csrWritePort    = new CsrWriteNdPort
+  val branchSuccess   = Bool()
 
   val exeOp  = UInt(Param.Width.exeOp)
   val exeSel = UInt(Param.Width.exeSel)
   val robId  = UInt(Param.Width.Rob.id)
-
-  val branchSetPort = Output(new PcSetPort)
 
   val load    = if (isDiffTest) Some(new DifftestLoadNdPort) else None
   val store   = if (isDiffTest) Some(new DifftestStoreNdPort) else None
@@ -35,40 +34,61 @@ class InstInfoNdPort extends Bundle {
 }
 
 object InstInfoNdPort {
-  def default = (new InstInfoNdPort).Lit(
-    _.isValid -> false.B,
-    _.pc -> zeroWord,
-    _.inst -> zeroWord,
-    _.exceptionPos -> ExceptionPos.none,
-    _.exceptionRecord -> 0.U,
-    _.csrWritePort -> CsrWriteNdPort.default,
-    _.exeOp -> ExeInst.Op.nop,
-    _.exeSel -> ExeInst.Sel.none,
-    _.robId -> zeroWord,
-    _.isStore -> false.B,
-    _.vaddr -> zeroWord,
-    _.needCsr -> false.B,
-    _.branchSetPort -> PcSetPort.default,
-    _.load.get -> DifftestLoadNdPort.default,
-    _.store.get -> DifftestStoreNdPort.default,
-    _.tlbFill.get -> DifftestTlbFillNdPort.default,
-    _.isTlb -> false.B,
-    _.forbidParallelCommit -> false.B
-  )
+  def default = if (isDiffTest)
+    (new InstInfoNdPort).Lit(
+      _.isValid -> false.B,
+      _.pc -> zeroWord,
+      _.inst -> zeroWord,
+      _.exceptionPos -> ExceptionPos.none,
+      _.exceptionRecord -> 0.U,
+      _.csrWritePort -> CsrWriteNdPort.default,
+      _.exeOp -> ExeInst.Op.nop,
+      _.exeSel -> ExeInst.Sel.none,
+      _.robId -> zeroWord,
+      _.isStore -> false.B,
+      _.vaddr -> zeroWord,
+      _.needCsr -> false.B,
+      _.load.get -> DifftestLoadNdPort.default,
+      _.store.get -> DifftestStoreNdPort.default,
+      _.tlbFill.get -> DifftestTlbFillNdPort.default,
+      _.isTlb -> false.B,
+      _.forbidParallelCommit -> false.B,
+      _.branchSuccess -> false.B
+    )
+  else
+    (new InstInfoNdPort).Lit(
+      _.isValid -> false.B,
+      _.pc -> zeroWord,
+      _.inst -> zeroWord,
+      _.exceptionPos -> ExceptionPos.none,
+      _.exceptionRecord -> 0.U,
+      _.csrWritePort -> CsrWriteNdPort.default,
+      _.exeOp -> ExeInst.Op.nop,
+      _.exeSel -> ExeInst.Sel.none,
+      _.robId -> zeroWord,
+      _.isStore -> false.B,
+      _.vaddr -> zeroWord,
+      _.needCsr -> false.B,
+      _.isTlb -> false.B,
+      _.forbidParallelCommit -> false.B,
+      _.branchSuccess -> false.B
+    )
 
   def invalidate(instInfo: InstInfoNdPort): Unit = {
-    instInfo.isValid              := false.B
-    instInfo.needCsr              := false.B
-    instInfo.exceptionRecord      := 0.U
-    instInfo.exceptionPos         := ExceptionPos.none
-    instInfo.exeOp                := ExeInst.Op.nop
-    instInfo.exeSel               := ExeInst.Sel.none
-    instInfo.csrWritePort.en      := false.B
-    instInfo.load.get.en          := false.B
-    instInfo.store.get.en         := false.B
-    instInfo.branchSetPort.en     := false.B
+    instInfo.isValid         := false.B
+    instInfo.needCsr         := false.B
+    instInfo.exceptionRecord := 0.U
+    instInfo.exceptionPos    := ExceptionPos.none
+    instInfo.exeOp           := ExeInst.Op.nop
+    instInfo.exeSel          := ExeInst.Sel.none
+    instInfo.csrWritePort.en := false.B
+    if (isDiffTest) {
+      instInfo.load.get.en  := false.B
+      instInfo.store.get.en := false.B
+    }
     instInfo.isTlb                := false.B
     instInfo.isStore              := false.B
     instInfo.forbidParallelCommit := false.B
+    instInfo.branchSuccess        := false.B
   }
 }
