4851e39babeef42faf380b0b434d7eb6ef398a79
Takehana <cowpowermax@pm.me>
2023-07-05 21:34:02 +0800

    fix: cache last write assertion

--- src/src/memory/DCache.scala
+++ src/src/memory/DCache.scala
@@ -231,8 +231,10 @@ class DCache(
     val writeData      = UInt(Width.Mem.data)
     val writeMask      = UInt(Width.Mem.data)
     val isWrite        = Bool()
+    val writeDataLine  = Vec(Param.Count.DCache.dataPerLine, UInt(Width.Mem.data))
   }))
-  lastReg := lastReg // Fallback: Keep data
+  lastReg         := lastReg // Fallback: Keep data
+  lastReg.isWrite := false.B
   val last = new Bundle {
     val selectedStatusTag = WireDefault(lastReg.statusTagLines(lastReg.setIndex))
     val wbMemAddr = WireDefault(
@@ -317,6 +319,13 @@ class DCache(
       val selectedDataLine      = WireDefault(toDataLine(dataLines(setIndex)))
       val isCacheHit            = WireDefault(isSelectedVec.reduce(_ || _))
 
+      // Step 2: Read pass through (Last Step 2 is write)
+      val isLastMatched = reqMemAddr(Width.Mem._addr - 1, Param.Width.DCache._byteOffset) ===
+        lastReg.memAddr(Width.Mem._addr - 1, Param.Width.DCache._byteOffset)
+      when(isLastMatched && lastReg.isWrite) {
+        selectedDataLine := lastReg.writeDataLine
+      }
+
       // Step 2: Save data for later use
       lastReg.memAddr        := reqMemAddr
       lastReg.statusTagLines := statusTagLines
@@ -324,21 +333,9 @@ class DCache(
       lastReg.dataLine       := selectedDataLine
       lastReg.writeData      := reqWriteData
       lastReg.writeMask      := reqWriteMask
-      lastReg.isWrite        := readWriteReqReg === ReadWriteSel.write
 
       // Step 2: Select data by data index from byte offset
       val selectedData = WireDefault(selectedDataLine(dataIndex))
-
-      // Step 2: Read pass through (Last Step 2 is write)
-      val oldDataLast = WireDefault(selectedDataLine(dataIndex))
-      val newDataLast = WireDefault(writeWithMask(oldDataLast, lastReg.writeData, lastReg.writeMask))
-      when(
-        reqMemAddr(Width.Mem._addr - 1, log2Ceil(wordLength / byteLength)) ===
-          lastReg.memAddr(Width.Mem._addr - 1, log2Ceil(wordLength / byteLength)) &&
-          lastReg.isWrite
-      ) {
-        selectedData := newDataLast
-      }
 
       // Step 2: Whether hit or not
       when(isHasReqReg) {
@@ -358,6 +355,7 @@ class DCache(
             is(ReadWriteSel.write) {
               // Step 2: Write to cache (now hit)
 
+              lastReg.isWrite := true.B
               val writeStatusTag = WireDefault(selectedStatusTagLine)
 
               // Substitute write data in data line, with mask
@@ -367,6 +365,7 @@ class DCache(
                 case (data, index) =>
                   Mux(index.U === dataIndex, newData, data)
               }))
+              lastReg.writeDataLine := writeDataLine
 
               // Set dirty bit
               writeStatusTag.isDirty := true.B
