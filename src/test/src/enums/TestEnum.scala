41507d7df7027fa054f25539a1720a9ed9c208a7
Takehana <cowpowermax@pm.me>
2023-07-30 15:39:15 +0800

    fix: temporarily add additional stage in frontend for release package configuration

--- src/test/src/enums/TestEnum.scala
+++ src/test/src/enums/TestEnum.scala
@@ -1,7 +1,7 @@
-package enums
-
-import chisel3.ChiselEnum
-
-object TestEnum extends ChiselEnum {
-  val a, b, c = Value
-}
+//package enums
+//
+//import chisel3.ChiselEnum
+//
+//object TestEnum extends ChiselEnum {
+//  val a, b, c = Value
+//}
