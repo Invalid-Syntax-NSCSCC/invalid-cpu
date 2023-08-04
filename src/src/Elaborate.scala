5fc111cea8483b5517176741468dd1153d0cb4ba
Chrisqcwx <3075401625@qq.com>
2023-08-04 21:26:34 +0800

    fix: name error of CoreCpuTop

--- src/src/Elaborate.scala
+++ src/src/Elaborate.scala
@@ -2,7 +2,9 @@ import chisel3.stage._
 import circt.stage.{CIRCTTarget, CIRCTTargetAnnotation}
 
 object Elaborate extends App {
-  def top = new SimpleCoreCpuTop
+
+  class CoreCpuTop extends SimpleCoreCpuTop
+  def top = new CoreCpuTop
 
   val useMFC    = true // Use MLIR-based firrtl compiler
   val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
