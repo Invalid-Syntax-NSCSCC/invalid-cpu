94b0cb44c87c886f61bb3c2d96e220306567c216
Takehana <cowpowermax@pm.me>
2023-08-04 19:16:07 +0800

    refactor: rename class

--- src/src/Elaborate.scala
+++ src/src/Elaborate.scala
@@ -2,7 +2,7 @@ import chisel3.stage._
 import circt.stage.{CIRCTTarget, CIRCTTargetAnnotation}
 
 object Elaborate extends App {
-  def top = new CoreCpuTop
+  def top = new SimpleCoreCpuTop
 
   val useMFC    = true // Use MLIR-based firrtl compiler
   val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
