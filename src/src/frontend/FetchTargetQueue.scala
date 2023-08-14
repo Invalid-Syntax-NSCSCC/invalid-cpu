9e9d8dc843f6307eb6a355a93f693736a0ce4057
zhengyujie <3257144507@qq.com>
2023-08-14 16:29:13 +0800

    feat:reduce FTB cost

--- src/src/frontend/FetchTargetQueue.scala
+++ src/src/frontend/FetchTargetQueue.scala
@@ -242,16 +242,25 @@ class FetchTargetQueue(
   io.bpuFtqPort.ftqBpuTrainMeta.branchAddrBundle.fallThroughAddr := ftqBranchMetaRegs(commitFtqId).fallThroughAddr
 
   // commit to ras
-  io.ftqRasPort.valid         := RegNext(io.commitFtqTrainPort.isTrainValid, false.B)
-  io.ftqRasPort.bits.isPush   := RegNext(io.commitFtqTrainPort.branchTakenMeta.branchType === BranchType.call, false.B)
-  io.ftqRasPort.bits.isPop    := RegNext(io.commitFtqTrainPort.branchTakenMeta.branchType === BranchType.ret, false.B)
-  io.ftqRasPort.bits.callAddr := RegNext(ftqBranchMetaRegs(commitFtqId).fallThroughAddr, 0.U)
-  io.ftqRasPort.bits.predictError := RegNext(
+//  io.ftqRasPort.valid         := RegNext(io.commitFtqTrainPort.isTrainValid, false.B)
+//  io.ftqRasPort.bits.isPush   := RegNext(io.commitFtqTrainPort.branchTakenMeta.branchType === BranchType.call, false.B)
+//  io.ftqRasPort.bits.isPop    := RegNext(io.commitFtqTrainPort.branchTakenMeta.branchType === BranchType.ret, false.B)
+//  io.ftqRasPort.bits.callAddr := RegNext(ftqBranchMetaRegs(commitFtqId).fallThroughAddr, 0.U)
+//  io.ftqRasPort.bits.predictError := RegNext(
+//    ftqBranchMetaRegs(
+//      commitFtqId
+//    ).ftbDirty || (io.commitFtqTrainPort.branchTakenMeta.predictedTaken ^ io.commitFtqTrainPort.branchTakenMeta.isTaken),
+//    false.B
+//  )
+  io.ftqRasPort.valid         := io.commitFtqTrainPort.isTrainValid
+  io.ftqRasPort.bits.isPush   := io.commitFtqTrainPort.branchTakenMeta.branchType === BranchType.call
+  io.ftqRasPort.bits.isPop    := io.commitFtqTrainPort.branchTakenMeta.branchType === BranchType.ret
+  io.ftqRasPort.bits.callAddr := ftqBranchMetaRegs(commitFtqId).fallThroughAddr
+  io.ftqRasPort.bits.predictError :=
     ftqBranchMetaRegs(
       commitFtqId
-    ).ftbDirty || (io.commitFtqTrainPort.branchTakenMeta.predictedTaken ^ io.commitFtqTrainPort.branchTakenMeta.isTaken),
-    false.B
-  )
+    ).ftbDirty || (io.commitFtqTrainPort.branchTakenMeta.predictedTaken ^ io.commitFtqTrainPort.branchTakenMeta.isTaken)
+
 //  }
 
   // Bpu meta ram
@@ -317,12 +326,16 @@ class FetchTargetQueue(
     io.bpuFtqPort.ftqBpuTrainMeta.branchAddrBundle.jumpTargetAddr := io.exeFtqPort.feedBack.commitBundle.ftqMetaUpdateJumpTarget
     io.bpuFtqPort.ftqBpuTrainMeta.branchAddrBundle.fallThroughAddr := io.exeFtqPort.feedBack.commitBundle.ftqMetaUpdateFallThrough
 
-    io.ftqRasPort.bits.callAddr := RegNext(io.exeFtqPort.feedBack.commitBundle.ftqMetaUpdateFallThrough, 0.U)
-    io.ftqRasPort.bits.predictError := RegNext(
+//    io.ftqRasPort.bits.callAddr := RegNext(io.exeFtqPort.feedBack.commitBundle.ftqMetaUpdateFallThrough, 0.U)
+//    io.ftqRasPort.bits.predictError := RegNext(
+//      io.exeFtqPort.feedBack.commitBundle.ftqMetaUpdateFtbDirty ||
+//        (io.commitFtqTrainPort.branchTakenMeta.predictedTaken ^ io.commitFtqTrainPort.branchTakenMeta.isTaken),
+//      false.B
+//    )
+    io.ftqRasPort.bits.callAddr := io.exeFtqPort.feedBack.commitBundle.ftqMetaUpdateFallThrough
+    io.ftqRasPort.bits.predictError :=
       io.exeFtqPort.feedBack.commitBundle.ftqMetaUpdateFtbDirty ||
-        (io.commitFtqTrainPort.branchTakenMeta.predictedTaken ^ io.commitFtqTrainPort.branchTakenMeta.isTaken),
-      false.B
-    )
+        (io.commitFtqTrainPort.branchTakenMeta.predictedTaken ^ io.commitFtqTrainPort.branchTakenMeta.isTaken)
   }
 
 }
--- src/src/frontend/bpu/components/Bundles/FtbEntryNdPort.scala
+++ src/src/frontend/bpu/components/Bundles/FtbEntryNdPort.scala
@@ -7,15 +7,20 @@ class FtbEntryNdPort extends Bundle {
   val valid            = Bool()
   val isCrossCacheline = Bool()
   val branchType       = UInt(Param.BPU.BranchType.width.W)
+  val fetchLength      = UInt(log2Ceil(Param.fetchInstMaxNum + 1).W)
 
   // Virtual tag, pc[1:0] is always 0, so not used in index or tag
-  val tag             = UInt((spec.Width.Mem._addr - 2 - log2Ceil(Param.BPU.FTB.nset)).W)
-  val jumpTargetAddr  = UInt(spec.Width.Mem.addr)
-  val fallThroughAddr = UInt(spec.Width.Mem.addr)
+  val tag            = UInt(Param.BPU.FTB.tagWidth.W)
+  val jumpTargetAddr = UInt(spec.Width.Mem.addr)
+//  val fallThroughAddr = UInt(spec.Width.Mem.addr)
 }
 
 object FtbEntryNdPort {
+//  val width =
+//    1 + 1 + 2 + spec.Width.Mem._addr - 2 - log2Ceil(Param.BPU.FTB.nset) + 2 * spec.Width.Mem._addr
   val width =
-    1 + 1 + 2 + spec.Width.Mem._addr - 2 - log2Ceil(Param.BPU.FTB.nset) + 2 * spec.Width.Mem._addr
+    1 + 1 + Param.BPU.BranchType.width + log2Ceil(
+      Param.fetchInstMaxNum + 1
+    ) + Param.BPU.FTB.tagWidth + spec.Width.Mem._addr
   def default = 0.U.asTypeOf(new FtbEntryNdPort)
 }
