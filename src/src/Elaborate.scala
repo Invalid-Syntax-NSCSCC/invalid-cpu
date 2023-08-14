45eca9a296842a21a44e50afc64a5d2c0d838c12
Takehana <cowpowermax@pm.me>
2023-08-14 16:56:33 +0800

    fix: bad code

--- src/src/Elaborate.scala
+++ src/src/Elaborate.scala
@@ -4,18 +4,15 @@ import spec.Param
 
 object Elaborate extends App {
 
-  var generator: Seq[chisel3.stage.ChiselGeneratorAnnotation] = Seq()
-
-  if (Param.useSimpleBackend) {
+  val generator = if (Param.useSimpleBackend) {
     class CoreCpuTop extends SimpleCoreCpuTop
-    generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new CoreCpuTop))
+    Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new CoreCpuTop))
   } else {
     class CoreCpuTop extends ComplexCoreCpuTop
-    generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new CoreCpuTop))
+    Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new CoreCpuTop))
   }
 
   val useMFC = true // Use MLIR-based firrtl compiler
-  // val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
 
   if (useMFC) {
     (new circt.stage.ChiselStage)
