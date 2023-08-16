267c0fa74d2e5a19e89d07924fcb204c44fb7925
invalid1 <Jim-shop@users.noreply.github.com>
2023-08-16 16:31:16 +0800

    feat: LUTRAM for status-tag

---
+++ src/src/memory/LutRam.scala
@@ -0,0 +1,47 @@
+package memory
+
+import chisel3._
+import chisel3.util._
+
+class SingleLutRam(size: Int, dataWidth: Int) extends Module {
+  val addrWidth = log2Ceil(size)
+
+  val io = IO(new Bundle {
+    val isWrite = Input(Bool())
+    val addr    = Input(UInt(addrWidth.W))
+    val dataIn  = Input(UInt(dataWidth.W))
+    val dataOut = Output(UInt(dataWidth.W))
+  })
+
+  val data = RegInit(VecInit(Seq.fill(size)(0.U(dataWidth.W))))
+
+  // Read
+  io.dataOut := data(io.addr)
+
+  // Write
+  when(io.isWrite) {
+    data(io.addr) := io.dataIn
+  }
+}
+
+class SimpleDualLutRam(size: Int, dataWidth: Int) extends Module {
+  val addrWidth = log2Ceil(size)
+
+  val io = IO(new Bundle {
+    val isWrite   = Input(Bool())
+    val readAddr  = Input(UInt(addrWidth.W))
+    val writeAddr = Input(UInt(addrWidth.W))
+    val dataIn    = Input(UInt(dataWidth.W))
+    val dataOut   = Output(UInt(dataWidth.W))
+  })
+
+  val data = RegInit(VecInit(Seq.fill(size)(0.U(dataWidth.W))))
+
+  // Read
+  io.dataOut := data(io.readAddr)
+
+  // Write
+  when(io.isWrite) {
+    data(io.writeAddr) := io.dataIn
+  }
+}
