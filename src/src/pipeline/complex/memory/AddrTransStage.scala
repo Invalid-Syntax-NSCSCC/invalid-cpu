36fd70357a4674dfafdb6cf4bd782ed355d99c80
invalid1 <Jim-shop@users.noreply.github.com>
2023-08-16 21:43:18 +0800

    fix: cached strategy

--- src/src/pipeline/complex/memory/AddrTransStage.scala
+++ src/src/pipeline/complex/memory/AddrTransStage.scala
@@ -140,9 +140,16 @@ class AddrTransStage
   switch(transMode) {
     is(AddrTransType.direct) {
       translatedAddr := selectedInVirtAddr
+      when(peer.csr.crmd.datm === 0.U) {
+        out.isCached := false.B
+      }
     }
     is(AddrTransType.directMapping) {
       translatedAddr := Mux(directMapVec(0).isHit, directMapVec(0).mappedAddr, directMapVec(1).mappedAddr)
+      val isNotCached = Mux(directMapVec(0).isHit, peer.csr.dmw(0).mat === 0.U, peer.csr.dmw(1).mat === 0.U)
+      when(isNotCached) {
+        out.isCached := false.B
+      }
     }
     is(AddrTransType.pageTableMapping) {
       if (!isNoPrivilege) {
@@ -150,6 +157,9 @@ class AddrTransStage
         out.translatedMemReq.isValid := !peer.tlbTrans.exception.valid && selectedIn.memRequest.isValid
         out.cacheMaintenance.control.isL1Valid := !peer.tlbTrans.exception.valid && selectedIn.cacheMaintenance.control.isL1Valid
         out.cacheMaintenance.control.isL2Valid := !peer.tlbTrans.exception.valid && selectedIn.cacheMaintenance.control.isL2Valid
+        when(peer.tlbTrans.isNotCached) {
+          out.isCached := false.B
+        }
 
         // Handle exception
         val exceptionIndex = peer.tlbTrans.exception.bits
@@ -166,9 +176,6 @@ class AddrTransStage
   }
 
   // Can use cache
-  when(peer.csr.crmd.da && (peer.csr.crmd.datm === 0.U)) {
-    out.isCached := false.B
-  }
   if (isForcedCache) {
     out.isCached := true.B
   }
